d1c5874fab4a0dfd3f8a0325d8be76220114e889
David Fay <fay.david.a@gmail.com>
2017-03-14 05:48:05 -0400

    first commit

---
+++ app/src/main/java/com/fay/david/myfirstmapboxapp/MainActivity.java
@@ -0,0 +1,218 @@
+package com.fay.david.myfirstmapboxapp;
+
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.FloatingActionButton;
+import android.support.annotation.NonNull;
+import android.location.Location;
+import android.content.pm.PackageManager;
+import android.Manifest;
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
+import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationListener;
+import com.mapbox.mapboxsdk.location.LocationServices;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+import java.security.BasicPermission;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private MapView mapView;
+    private LocationServices locationServices;
+    private MapboxMap map;
+//    private FloatingActionButton floatingActionButton;
+//    private DatabaseReference mDatabase;
+
+    private static final int PERMISSIONS_LOCATION = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Call this to cache the database locally
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        //get an instance and reference to the database
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference mDatabaseRef = mDatabase.getReference("location");
+
+        MapboxAccountManager.start(this,getString(R.string.access_token));
+
+        setContentView(R.layout.activity_main);
+
+        locationServices = LocationServices.getLocationServices(MainActivity.this);
+
+        IconFactory iconFactory = IconFactory.getInstance(MainActivity.this);
+        Drawable iconDrawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.compass);
+        final Icon icon = iconFactory.fromDrawable(iconDrawable);
+
+        //create a mapview
+        mapView = (MapView) findViewById(R.id.mapview);
+
+        mapView.onCreate(savedInstanceState);
+        final MarkerViewOptions home = new MarkerViewOptions()
+                .position(new LatLng(39.700931, -83.743719)).title("Our House").snippet("I live here");
+        final MarkerViewOptions compass = new MarkerViewOptions().icon(icon).position(new LatLng(0,0)).title("moving marker").snippet("watch me go");
+
+
+
+        // Add a MapboxMap
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap){
+                map = mapboxMap;
+
+                // customize map with markers, polylines etc
+                map.setStyleUrl(Style.SATELLITE_STREETS);
+
+                map.addMarker(home);
+
+            }
+        });
+
+        // Read from the database
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Double lat = dataSnapshot.child("testuser").child("lat").getValue(Double.class);
+                Double lng = dataSnapshot.child("testuser").child("lng").getValue(Double.class);
+
+                map.addMarker(compass).setPosition(new LatLng(lat, lng));
+
+                Log.d(TAG, "Value is: " + lat + " " + lng);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+        /*floatingActionButton = (FloatingActionButton) findViewById(R.id.location_toggle_fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (map != null) {
+                    toggleGps(!map.isMyLocationEnabled());
+                }
+            }
+        });*/
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+
+    }
+
+    @Override
+    public void onLowMemory(){
+        super.onLowMemory();
+        mapView.onLowMemory();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+
+    }
+
+    private void toggleGps(boolean enableGps) {
+        if (enableGps) {
+            // Check if user has granted location permission
+            if (!locationServices.areLocationPermissionsGranted()) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_LOCATION);
+            } else {
+                enableLocation(true);
+            }
+        } else {
+            enableLocation(false);
+        }
+    }
+
+    private void enableLocation(boolean enabled) {
+        if (enabled) {
+            // If we have the last location of the user, we can move the camera to that position.
+            Location lastLocation = locationServices.getLastLocation();
+            if (lastLocation != null) {
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastLocation), 16));
+            }
+
+            locationServices.addLocationListener(new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    if (location != null) {
+                        // Move the map camera to where the user location is and then remove the
+                        // listener so the camera isn't constantly updating when the user location
+                        // changes. When the user disables and then enables the location again, this
+                        // listener is registered again and will adjust the camera once again.
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location), 16));
+                        locationServices.removeLocationListener(this);
+                    }
+                }
+            });
+//            floatingActionButton.setImageResource(R.drawable.ic_location_disabled_24dp);
+        } else {
+//            floatingActionButton.setImageResource(R.drawable.ic_my_location_24dp);
+        }
+        // Enable or disable the location layer on the map
+        map.setMyLocationEnabled(enabled);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableLocation(true);
+            }
+        }
+    }
+
+
+
+
+}
